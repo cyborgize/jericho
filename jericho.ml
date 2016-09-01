df4e52749bcc4d70089f57131a067fbc5f9b467c
Raman Varabets <roman.vorobets@gmail.com>
2016-09-01 23:01:12 +0800

    support pretty-printing in set and update

--- jericho.ml
+++ jericho.ml
@@ -154,15 +154,25 @@ let event_stream url =
 
 let make ~auth base_url =
   let log_error ?exn action path error = log #error ?exn "%s %s : %s" (Web.string_of_http_action action) path error in
-  let query action ?(args=[]) ?print path data =
-    let body = match data with Some data -> Some ("application/json", Yojson.Safe.to_string data) | None -> None in
+  let query action ?(pretty=false) ?(args=[]) ?print path data =
+    let body =
+      match data with
+      | None -> None
+      | Some data ->
+      let data =
+        match pretty with
+        | true -> Yojson.Safe.pretty_to_string ~std:true data
+        | false -> Yojson.Safe.to_string ~std:true data
+      in
+      Some ("application/json", data)
+    in
     let args = ("auth", Some auth) :: ("print", Option.map string_of_print print) :: args in
     let args = List.filter_map (function (k, Some v) -> Some (k, v) | _ -> None) args in
     let url = sprintf "%s%s.json?%s" base_url path (Web.make_url_args args) in
     Web.http_query_lwt ~verbose:(log #level = `Debug) ?body action url
   in
-  let bool_query action path data =
-    match%lwt query action ~print:`Silent path data with
+  let bool_query action ?pretty path data =
+    match%lwt query action ?pretty ~print:`Silent path data with
     | `Ok _ -> Lwt.return_true
     | `Error error -> log_error action path error; Lwt.return_false
   in
@@ -180,9 +190,9 @@ let make ~auth base_url =
       ] in
       query `GET ~args ?print k None
 
-    method set k v = bool_query `PUT k (Some v)
+    method set ?pretty k v = bool_query `PUT ?pretty k (Some v)
 
-    method update k v = bool_query `PATCH k (Some v)
+    method update ?pretty k v = bool_query `PATCH ?pretty k (Some v)
 
     method update_multi k l = bool_query `PATCH k (Some (`Assoc l))
 
